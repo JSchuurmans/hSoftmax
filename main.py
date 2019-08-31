726fdcdc8974bd81301dc28edfe24dbc61a3137a
JSchuurmans <jetzeschuurmans@gmail.com>
2019-08-31 15:50:01 +0200

    results and param logging

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from time import time
 # import matplotlib.pyplot as plt 
 import torch
 import numpy as np 
+import pandas as pd
 
 from src.util import Trainer, Loader
 from src.lstm import LSTM
@@ -22,14 +23,17 @@ parser.add_argument('-bi', action='store_true')
 
 args = parser.parse_args()
 
+param = dict()
+
 data = args.data.lower()
 if data not in DATASETS:
     raise NotImplementedError
-
+param['data'] = data
 print(f'Dataset: {args.data}')
 
 model_name = 'hLSTM' if args.hi else 'LSTM'
 print(f'Model: {model_name}')
+param['model'] = model_name
 
 # TODO work this out per dataset, per model
 # model parameters
@@ -39,11 +43,19 @@ wdim = 300 # word embedding dimension {50, 100, 200, 300}
 word_path = os.path.join('wordvectors',f'glove.6B.{wdim}d.txt')
 hdim = 150  # hidden dimension
 bidir = args.bi # bidirectional
+param['hier'] = hier
+param['dropout'] = dropout
+param['wdim'] = wdim
+param['hdim'] = hdim
+param['bidir'] = bidir
 
 # training parameters
 lr = 0.001 # learning rate
 bs = 10 # batch size
-n_epochs = 25
+n_epochs = 10
+param['lr'] = lr
+param['bs'] = bs
+param['n_epochs'] = n_epochs
 
 log_dir = os.path.join('log',data,model_name,str(time()))
 if not os.path.exists(log_dir):
@@ -85,12 +97,15 @@ elif data == 'ya':
     n_max = 30
     # n_max = 29
 elif data == 'ya_16':
-    dataset_path = os.path.join('data','ya','Yahoo','Yahoo.ESA')
+    dataset_path = os.path.join('data','ya','Yahoo','Yahoo.ESA_16_2')
     train_data, test_data, mappings = loader.load_ya(dataset_path,
                     word_path, wdim, hier=hier, data_name=data)
     n_cat = 16
     n_max = 30
     # n_max = 29
+
+param['n_cat'] = n_cat
+param['n_max'] = n_max
     
 word_to_id = mappings['word_to_id']
 tag_to_id = mappings['tag_to_id']
@@ -121,14 +136,51 @@ losses, all_A, all_P, all_R, all_F = trainer.train_model(n_epochs, train_data,
                                     checkpoint_path=log_dir)
 
 
-F1_train = all_F[-1][0]
-F1_test = all_F[-1][1]
+A_train = all_A[-1][0]
+A_test = all_A[-1][1]
+
+F_train = all_F[-1][0]
+F_test = all_F[-1][1]
 
 P_train = all_P[-1][0]
 P_test = all_P[-1][1]
 
 R_train = all_R[-1][0]
 R_test = all_R[-1][1]
+
+A_train_all = [X[0] for X in all_A]
+A_test_all = [X[1] for X in all_A]
+
+F_train_all = [X[0] for X in all_F]
+F_test_all = [X[1] for X in all_F]
+
+P_train_all = [X[0] for X in all_P]
+P_test_all = [X[1] for X in all_P]
+
+R_train_all = [X[0] for X in all_R]
+R_test_all = [X[1] for X in all_R]
+
+train_metrics = pd.DataFrame({
+    'A': A_train_all,
+    'F': F_train_all,
+    'P': P_train_all,
+    'R': R_train_all
+})
+train_metrics.to_csv(os.path.join(log_dir,'train_metrics.csv'))
+train_metrics.to_pickle(os.path.join(log_dir,'train_metrics.pkl'))
+
+test_metrics = pd.DataFrame({
+    'A': A_train_all,
+    'F': F_train_all,
+    'P': P_train_all,
+    'R': R_train_all
+})
+test_metrics.to_csv(os.path.join(log_dir,'test_metrics.csv'))
+test_metrics.to_pickle(os.path.join(log_dir,'test_metrics.pkl'))
+
+param_df = pd.DataFrame({param})
+param_df.to_csv(os.path.join(log_dir, 'param.csv'))
+param_df.to_pickle(os.path.join(log_dir,'param.pkl'))
 
 # TODO save model parameters
 # TODO save metrics
